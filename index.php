f53c8f6d9cdefbbe4b4e967f69c4e836c7b0ed25
Postwaiter <postwaiter@mail.ru>
2015-03-27 20:32:09 +0300

    Starting MVC project

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<html>
-<head>
-	<title>Art Me Please</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-</head>
-<body>
-
-	<h1>New Savchenko</h1>
- 
-	<h2>Memcached</h2>
-	<?php
-	$memcache = new Memcache;
-	$memcache->connect('127.0.0.1', 11211);
-	echo 'Memcached ver: ' . $memcache->getVersion();
-	?>
-
-
-</body>
-</html>
-
-
-
-
+<?php
+    header("Content-Type: text/html; charset=utf-8");
+    ini_set('display_errors', 1);
+    require_once 'application/bootstrap.php';
