ed3513704a729db1dc6d55d3dea48f7d357f802f
Postwaiter <postwaiter@mail.ru>
2015-03-30 01:48:28 +0300

    SourceTree logo added

--- application/models/model_abilities.php
+++ application/models/model_abilities.php
@@ -12,8 +12,13 @@ class Model_Portfolio extends Model
             ),
             array(
                 'Soft' => '<a href="https://netbeans.org/"><img src="images\netBeans_logo.png" alt="логотип"></a>',
-                'Description' => 'NetBeans IDE lets you quickly and easily develop Java desktop, mobile, and web applications, as well as HTML5 applications with HTML, JavaScript, and CSS. The IDE also provides a great set of tools for PHP and C/C++ developers. It is free and open source and has a large community of users and developers around the world. '
+                'Description' => 'NetBeans IDE lets you quickly and easily develop Java desktop, mobile, and web applications, as well as HTML5 applications with HTML, JavaScript, and CSS. The IDE also provides a great set of tools for PHP and C/C++ developers. It is free and open source and has a large community of users and developers around the world. git '
             ),
+            array(
+                'Soft' => '<a href="http://sourcetreeapp.com/"><img src="images\sourceTree_logo.png" alt="логотип"></a>',
+                'Description' => 'A free Git & Mercurial client for Windows or Mac'
+            ),
+
             // todo
         );
     }
