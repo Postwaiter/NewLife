43e7c0ca38faa7157fb471c501ce1436926122bd
Postwaiter <postwaiter@mail.ru>
2015-04-05 09:49:32 +0300

    Protocols added

--- application/models/model_refs.php
+++ application/models/model_refs.php
@@ -7,6 +7,16 @@ class Model_Refs extends Model
         return array(
             
             array(
+                'Concept' => 'Протоколы',
+                'Ref' => '<a href="https://ru.wikipedia.org/wiki/HTTP">HTTP</a>, <a href="https://ru.wikipedia.org/wiki/TCP/IP">TCP/IP</a>, <a href="https://ru.wikipedia.org/wiki/SSH">SSH</a>, <a href="https://ru.wikipedia.org/wiki/SSL">SSL<br/>'
+                . '<a href="http://habrahabr.ru/post/215117/">HTTP(Habrhabr)</a>'
+            ),
+            array(
+                
+                'Concept' => 'http://ZopoMobile.ru',
+                'Ref' => 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'
+            ),
+                        array(
                 'Concept' => 'http://DunkelBeer.ru',
                 'Ref' => 'Промо-сайт темного пива Dunkel от немецкого производителя Löwenbraü выпускаемого в России пивоваренной компанией "CАН ИнБев".'
             ),
@@ -15,7 +25,15 @@ class Model_Refs extends Model
                 'Concept' => 'http://ZopoMobile.ru',
                 'Ref' => 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'
             ),
-            // todo
+                        array(
+                'Concept' => 'http://DunkelBeer.ru',
+                'Ref' => 'Промо-сайт темного пива Dunkel от немецкого производителя Löwenbraü выпускаемого в России пивоваренной компанией "CАН ИнБев".'
+            ),
+            array(
+                
+                'Concept' => 'http://ZopoMobile.ru',
+                'Ref' => 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'
+            ),
         );
     }
 }
