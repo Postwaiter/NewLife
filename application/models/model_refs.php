69856bbf62dfb9ad87cc2ddc7b6e9f3446e05c3c
Postwaiter <postwaiter@mail.ru>
2015-04-05 03:18:44 +0300

    refact

--- application/models/model_refs.php
+++ application/models/model_refs.php
@@ -7,15 +7,13 @@ class Model_Refs extends Model
         return array(
             
             array(
-                'Year' => '2012',
-                'Site' => 'http://DunkelBeer.ru',
-                'Description' => 'Промо-сайт темного пива Dunkel от немецкого производителя Löwenbraü выпускаемого в России пивоваренной компанией "CАН ИнБев".'
+                'Concept' => 'http://DunkelBeer.ru',
+                'Ref' => 'Промо-сайт темного пива Dunkel от немецкого производителя Löwenbraü выпускаемого в России пивоваренной компанией "CАН ИнБев".'
             ),
             array(
                 
-                'Year' => '2014',
-                'Site' => 'http://ZopoMobile.ru',
-                'Description' => 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'
+                'Concept' => 'http://ZopoMobile.ru',
+                'Ref' => 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'
             ),
             // todo
         );
--- application/views/refs_view.php
+++ application/views/refs_view.php
@@ -2,12 +2,12 @@
 <p>
             <table>
             Все проекты в следующей таблице являются вымышленными, поэтому даже не пытайтесь перейти по приведенным ссылкам.
-            <tr><td>Год</td><td>Проект</td><td>Описание</td></tr>
+            <tr><td>Понятие</td><td>Ссылка</td></tr>
             <?php
 
                 foreach($data as $row)
                 {
-                    echo '<tr><td>'.$row['Year'].'</td><td>'.$row['Site'].'</td><td>'.$row['Description'].'</td></tr>';
+                    echo '<tr><td>'.$row['Concept'].'</td><td>'.$row['Ref'].'</td></tr>';
                 }
 
             ?>
